11e72d93bfdfd7ed67ca2097eb2af2c84072087b
pistenfeger <pistenfeger.c@gmail.com>
2021-08-25 20:24:21 +0200

    added Pieces to Part

--- objects.js
+++ objects.js
@@ -20,38 +20,74 @@ class Part {
             case 1:
                 //L (4 Pieces)
                 //                         X  Y
+                this.pieces.push(new Piece(0, 0));
+                this.pieces.push(new Piece(0, 1));
+                this.pieces.push(new Piece(0, 2));
+                this.pieces.push(new Piece(1, 2));
                 break;
             case 2:
                 //L (Inverted) (4 Pieces)
                 //                         X  Y
+                this.pieces.push(new Piece(1, 0));
+                this.pieces.push(new Piece(1, 1));
+                this.pieces.push(new Piece(1, 2));
+                this.pieces.push(new Piece(0, 2));
                 break;
             case 3:
                 //I (3 Pieces)
                 //                         X  Y
+                this.pieces.push(new Piece(0, 0));
+                this.pieces.push(new Piece(0, 1));
+                this.pieces.push(new Piece(0, 2));
                 break;
             case 4:
                 //I (4 Pieces)
                 //                         X  Y
+                this.pieces.push(new Piece(0, 0));
+                this.pieces.push(new Piece(0, 1));
+                this.pieces.push(new Piece(0, 2));
+                this.pieces.push(new Piece(0, 3));
                 break;
             case 5:
                 //I (5 Pieces)
                 //                         X  Y
+                this.pieces.push(new Piece(0, 0));
+                this.pieces.push(new Piece(0, 1));
+                this.pieces.push(new Piece(0, 2));
+                this.pieces.push(new Piece(0, 3));
+                this.pieces.push(new Piece(0, 4));
                 break;
             case 6:
                 //T (4 Pieces)
                 //                         X  Y
+                this.pieces.push(new Piece(0, 0));
+                this.pieces.push(new Piece(1, 0));
+                this.pieces.push(new Piece(2, 0));
+                this.pieces.push(new Piece(1, 1));
                 break;
             case 7:
                 //T (5 Pieces)
                 //                         X  Y
+                this.pieces.push(new Piece(0, 0));
+                this.pieces.push(new Piece(1, 0));
+                this.pieces.push(new Piece(2, 0));
+                this.pieces.push(new Piece(1, 1));
+                this.pieces.push(new Piece(1, 2));
                 break;
             case 8:
                 //L (3 Pieces)
                 //                         X  Y
+                this.pieces.push(new Piece(0, 0));
+                this.pieces.push(new Piece(0, 1));
+                this.pieces.push(new Piece(0, 2));
                 break;
             case 9:
                 //o (4 Pieces)
                 //                         X  Y
+                this.pieces.push(new Piece(0, 0));
+                this.pieces.push(new Piece(1, 0));
+                this.pieces.push(new Piece(0, 1));
+                this.pieces.push(new Piece(1, 1));
                 break;
         }
     }
